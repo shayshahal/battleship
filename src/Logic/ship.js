73feb1b9c0b364ed0de8bc4bd40761e1d722dd4e
shayshahal <shay840@gmail.com>
2023-01-09 19:31:28 +0200

    fixed hit function

--- src/Logic/ship.js
+++ src/Logic/ship.js
@@ -1,6 +1,7 @@
 export default ship = (len) => {
+    const length = len;
     let numOfTimesHit = 0;
-    const hit = () => {numOfTimesHit++};
-    const isSunk = () => numOfTimesHit >= len;
+    const hit = () => numOfTimesHit++;
+    const isSunk = () => numOfTimesHit >= length;
     return {hit, isSunk}
 }
