26737d80b8612365977bc62c75136b74aed3b919
shayshahal <shay840@gmail.com>
2023-01-09 20:36:24 +0200

    fixed matrix calls ([][] instead of [,])

--- src/Logic/gameBoard.js
+++ src/Logic/gameBoard.js
@@ -3,51 +3,56 @@ export default gameBoard = () =>{
     let board = Array.from(Array(10), ()=> {
         return Array.from(Array(10), ()=>{return {ship: null, checked: false}})
     });
-    // Initialize counter that tracks how many ships are alive on the board, index+1 = length of ship
-    let counter = [4, 3, 2, 1];
+    // Initialize counter that tracks how many ships are alive on the board
+    let counter = 0;
     const checkValidity = (ship, coor) => {
         // Check if out of bounds
-        if(coor.x > 9 || coor.x < 0 || coor.y > 9 || coor.y < 0)
+        if(outOfBounds(coor))
             return false;
         // Runs on adjacent coordinates
         for (let i = -1; i < 2; i++) {
             for (let k = -1; k < 2; k++){
                 const curr = board[coor.x+i] ? board[coor.x+i][coor.y+k] : null;
                 // If spot is out of bounds or
-                if(curr){
+                if(curr && curr.ship){
                     /* Checks if 
                         1.coordinates are not already occupied or diagonally adjacent to an occupied spot.
-                        2.coordinates are adjacent to another ship
+                        2.coordinates are adjacent to a different ship
                     */
-                    if((((i === 0 && k === 0) || (i !== 0 && k !== 0)) && curr.ship) || (ship??curr.ship) !== ship)
+                    if(((i === 0 && k === 0) || (i !== 0 && k !== 0)) || curr.ship !== ship)
                         return false;
                 }
             }        
         }
         return true;
     }
-    const removeShip = (coor) => board[coor.x, coor.y].ship = null;
+    const outOfBounds = (coor) => (coor.x > 9 || coor.x < 0 || coor.y > 9 || coor.y < 0);
+
+    const removeShip = (coor) => board[coor.x][coor.y].ship = null;
+    
     const placeShip = (ship, coor) => {
         if(checkValidity(ship, coor)){
-            board[coor.x, coor.y].ship = ship;
+            board[coor.x][coor.y].ship = ship;
             return true;
         }
         return false;
     }
-    const isDestroyed = () => counter.every(s=> !s);
+    const isDestroyed = () => counter === 10;
     
     const receiveAttack = (coor) =>{
-        board[coor.x, coor.y].checked = true;
-        if(board[coor.x, coor.y].ship){
-            board[coor.x, coor.y].ship.hit();
-            if(board[coor.x+1, coor.y+1])
-                board[coor.x+1, coor.y+1].checked = true;
-            if(board[coor.x+1, coor.y-1])
-                board[coor.x+1, coor.y-1].checked = true;
-            if(board[coor.x-1, coor.y+1])
-                board[coor.x-1, coor.y+1].checked = true;
-            if(board[coor.x-1, coor.y-1])
-                board[coor.x-1, coor.y-1].checked = true;
+        board[coor.x][coor.y].checked = true;
+        if(board[coor.x][coor.y].ship){
+            board[coor.x][coor.y].ship.hit();
+            if(!outOfBounds({x: coor.x+1, y: coor.y+1}))
+                board[coor.x+1][coor.y+1].checked = true;
+            if(!outOfBounds({x: coor.x+1, y: coor.y-1}))
+                board[coor.x+1][coor.y-1].checked = true;
+            if(!outOfBounds({x: coor.x-1, y: coor.y+1}))
+                board[coor.x-1][coor.y+1].checked = true;
+            if(!outOfBounds({x: coor.x-1, y: coor.y-1}))
+                board[coor.x-1][coor.y-1].checked = true;
+            if(board[coor.x][coor.y].ship.isSunk())
+                counter++;
             return true;
         }
         return false;
--- src/__tests__/gameBoard.test.js
+++ src/__tests__/gameBoard.test.js
@@ -11,7 +11,7 @@ let board, shipArr = new Array(4);
 
 describe('gameBoard tests', () => { 
     
-    describe('ship placing tests', () => { 
+    describe('ship placing/removing tests', () => { 
         test('should fail to place a outside of bounds', () => {
             let rndX = ~~(Math.random*20);
             let rndY = rndX - 10;
@@ -21,10 +21,16 @@ describe('gameBoard tests', () => {
         test('should place a part of a ship successfully', () => { 
             expect(board.placeShip(shipArr[1][0], {x: 1, y:1})).toBeTruthy();
         });
-
+        test('should fail to place a second part of the ship on the same spot', () => {
+            expect(board.placeShip(shipArr[1][0], {x: 1, y:1})).toBeFalsy();
+        })
+        test('should place a part of a ship successfully on the same spot after removal', () => {
+            board.removeShip({x: 1, y:1});   
+            expect(board.placeShip(shipArr[1][0], {x: 1, y:1})).toBeTruthy();
+        });
         test('should fail to place a second part of the ship diagonally', () => {
-            let offsetX = 1 - 2*~~(Math.random*2);
-            let offsetY = 1 - 2*~~(Math.random*2);
+            let offsetX = 1 - (2*(~~(Math.random*2)));
+            let offsetY = 1 - (2*(~~(Math.random*2)));
             expect(board.placeShip(shipArr[1][0], {x: 1 + offsetX, y:1 + offsetY})).toBeFalsy();
         })
 
@@ -73,27 +79,27 @@ describe('gameBoard tests', () => {
 
         // Destroy all board but one
         board.receiveAttack({x: 9, y: 9});
-        board.receiveAttack({x: 0, y: 9});
         board.receiveAttack({x: 9, y: 0});
+        board.receiveAttack({x: 1, y: 5});
         for (let i = 0; i < 3; i++)
             board.receiveAttack({x: 3+i, y: 9});
         for (let i = 0; i < 3; i++)
-            board.receiveAttack({x: 9, y: 3+i});
+            board.receiveAttack({x: 9, y: 2+i});
         for (let i = 0; i < 2; i++)
-            board.receiveAttack({x: 0, y: 0+i});
+            board.receiveAttack({x: 0+i, y: 1});
         for (let i = 0; i < 2; i++)
             board.receiveAttack({x: 9, y: 6+i});
         for (let i = 0; i < 2; i++)
             board.receiveAttack({x: 4, y: 6+i});
         for (let i = 0; i < 3; i++)
-            board.receiveAttack({x: 0, y: 3+i});
+            board.receiveAttack({x: 0, y: 6-i});
 
         test('check if game is done again, should still be false', () => { 
             expect(board.isDestroyed()).toBeFalsy()
         })
 
         // Destroy the last part of the big piece
-        board.receiveAttack({x: 0, y: 6});
+        board.receiveAttack({x: 0, y: 3});
         
         test('check if game is done again, should be true', () => { 
             expect(board.isDestroyed()).toBeTruthy()
