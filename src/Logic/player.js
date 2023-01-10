cdca83fd7343aaa518f45fc8bb914076e4fd1c3c
shayshahal <shay840@gmail.com>
2023-01-10 19:36:34 +0200

    fnished player

---
+++ src/Logic/player.js
@@ -0,0 +1,16 @@
+import gameBoard from "./gameBoard"; 
+export default player = () =>{
+    let board = gameBoard();
+    let moves = Array.from(Array(3),()=> {return new Array(3).fill(false)})
+    const attack = (enemy, coor = null) => enemy.board.receiveAttack(coor??generateAttack());
+    const generateAttack =  () =>{
+        let rndX = ~~(Math.random*10), rndY = ~~(Math.random*10);
+        while(moves[rndX][rndY])
+            rndX = ~~(Math.random*10), rndX = ~~(Math.random*10);
+        return {x: rndX, y: rndY}
+    }
+    return {
+        get board(){return board;}, 
+        attack
+    }
+}
