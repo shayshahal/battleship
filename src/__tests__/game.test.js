1e66ec9ff574e01b1de78c16bf739685db647a22
shayshahal <shay840@gmail.com>
2023-01-12 02:44:29 +0200

    added game testing

--- src/__tests__/game.test.js
+++ src/__tests__/game.test.js
@@ -1,11 +1,35 @@
 import game from "../Logic/game";
-import player from "../Logic/player";
 
 describe('game loop tests', () => {
     let newGame;
     beforeAll(()=>{
         newGame = game();
     });
-    test('should make p1 attack p2', () => {})
+    test('should start with placing status', () => {
+        expect(newGame.status).toMatch('placing')
+    });
+    test('player and ai should exist', ()=>{
+        expect(newGame.player).toBeInstanceOf(Object);
+        expect(newGame.ai).toBeInstanceOf(Object);
+    })
+    test('should try to start game and fail', () => {
+        expect(newGame.start()).toBeFalsy();
+        expect(newGame.status).toMatch('placing');
+    })
+    let arr, arr2;
+    test('should try to start game and succeed', () => {
+        arr = newGame.ai.generatePlacement();
+        arr2 = newGame.player.generatePlacement();
+        expect(newGame.start()).toBeTruthy();
+        expect(newGame.status).toMatch("playing");
+    })
+    test('should finish game and change status to win', () => {
+        for (let i = 0; i < 10; i++) 
+            for (let k = 0; k < 10; k++)
+                if(arr[i][k] > 0)
+                    newGame.player.attack(newGame.ai, {x: i, y: k});
+        expect(newGame.isFinished()).toBeTruthy();
+        expect(newGame.status).toBe("won");
+    })
     
 })
