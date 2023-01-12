05256ff9c94727eaf5c7482c206cc807be7333ca
shayshahal <shay840@gmail.com>
2023-01-12 02:44:49 +0200

    added game factory

--- src/Logic/game.js
+++ src/Logic/game.js
@@ -1,3 +1,34 @@
+import newPlayer from "./player";
 export default game = () =>{
-    
+    let status = 'placing';
+    let player = newPlayer();
+    let ai = newPlayer();
+
+    const start = () =>{
+        if(!ai.isReady() || !player.isReady()){
+            return false;
+        }
+        status = 'playing'
+        return true;
+    }
+
+    const isFinished = () =>{
+        if(ai.board.isDestroyed()){
+            status = 'won';
+            return true;
+        }
+        if(player.board.isDestroyed()){
+            status = 'lost';
+            return false;
+        }
+        return false;
+    }
+
+    return {
+        get status(){return status}, 
+        get player(){return player}, 
+        get ai(){return ai},
+        start,
+        isFinished
+    }
 }
