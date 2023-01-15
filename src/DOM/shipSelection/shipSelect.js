4b88629455568b913dbaff61f2fa3c48b0006412
shayshahal <shay840@gmail.com>
2023-01-15 19:57:05 +0200

    added a ship selection module

---
+++ src/DOM/shipSelection/shipSelect.js
@@ -0,0 +1,30 @@
+import './shipSelect.css'
+const div = document.getElementById('extra-space');
+
+export default function createSelection(){
+    while(div.firstChild)
+        div.removeChild(div.firstChild);
+    let arr = [];
+    for (let i = 4; i > 0; i--) 
+        for(let k = 0; k <= 4-i; k++)
+            arr.push({radio: createShip(i), len: i});  
+    return arr;
+}
+
+function createShip(len){
+    const label = document.createElement('label');
+    label.for = 'ship' + len;
+    const radio = document.createElement('input');
+    radio.type = 'radio';
+    radio.id = len;
+    radio.name = 'ship';
+    label.append(radio);
+    for(let i = 0; i < len; i++){
+        const square = document.createElement('div');
+        square.classList.add('shipPiece')
+        square.classList.add('ship');
+        label.append(square);
+    }
+    div.append(label);
+    return label;
+}
