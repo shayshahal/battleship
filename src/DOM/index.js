2d415c222efb78e81db620ec8b416ffec56efaa9
shayshahal <shay840@gmail.com>
2023-01-16 18:29:01 +0200

    .

--- src/DOM/index.js
+++ src/DOM/index.js
@@ -1,28 +1,22 @@
-import createBoard from './board/board'
+import {createBoard} from './board/board'
 import {game} from '../Logic/game'
 import createSelection from './shipSelection/shipSelect';
 import './style.css'
 
-const playerBoardArr = createBoard();
-let newGame;
+const board = createBoard();
+let newGame = game();
 const ctrlBtn =document.getElementById('ctrl-btn');
 
-newGame = game();
-createSelection();
+const shipSelectArr = createSelection();
 
-for(let x = 0; x < 10; x++){
-    for (let y = 0; y < 10; y++) {
-        playerBoardArr[x][y].addEventListener('mousedown', ()=>{
-        if(playerBoardArr[x][y].classList.toggle('ship'))
-            newGame.player.board.placeShip(currShip, {'x': x, 'y': y});
-        else
-            newGame.player.returnShip(game.player.board.removeShip({'x': x, 'y': y}));
-        })      
-    }
-}
+// Ship taking events
+
+
+
+
 
 ctrlBtn.addEventListener('click', ()=>{
-    if(game.status === 'placing')
+    if(newGame.status === 'placing')
         newGame.start();
     else
         newGame = newGame();
