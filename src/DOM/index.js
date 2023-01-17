376503b8a77bfe648e1c43af2af53561f2884b6a
shayshahal <shay840@gmail.com>
2023-01-17 22:24:50 +0200

    added an init function to initialize the placement layout

--- src/DOM/index.js
+++ src/DOM/index.js
@@ -7,10 +7,31 @@ const board = createBoard();
 let newGame = game();
 const ctrlBtn =document.getElementById('ctrl-btn');
 
+init();
+
+function init(){
+    game = newGame();
+    transitionClear();
 const shipSelectArr = createSelection();
-
+    extraBoard.classList.add('ships');
+    btnsDiv.append(rndBtn);
+    btnsDiv.append(clrBtn);
+    clrBtn.click();
+    let currSelect;
 // Ship taking events
+    shipSelectArr.forEach((s) => {
+        s.radio.addEventListener('change', ()=>{
+            currSelect = s;
+            board.enableStack(currSelect.stack, currSelect.len === currSelect.stack.length);
+        })
+    });
 
+    shipSelectArr[0].radio.click();
+    // Board placing events
+    for(let x = 0; x < 10; x++)
+        for (let y = 0; y < 10; y++) 
+            board.arr[x][y].addEventListener('mousedown', ()=>{placeShipOnBoard(x, y, currSelect)})      
+}
 
 
 
