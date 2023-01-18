e0cd7f7fadd070ccef6c52241876396fe3f7b56d
shayshahal <shay840@gmail.com>
2023-01-18 21:50:31 +0200

    removed useless attribute in the objects of the board array

--- src/Logic/gameBoard.js
+++ src/Logic/gameBoard.js
@@ -1,7 +1,7 @@
 export const gameBoard = () =>{
     // Initialize board
     let board = Array.from(Array(10), ()=> {
-        return Array.from(Array(10), ()=>{return {ship: null, checked: false}})
+        return Array.from(Array(10), ()=>{})
     });
     // Initialize counter that tracks how many ships are alive on the board
     let counter = 0;
@@ -14,12 +14,12 @@ export const gameBoard = () =>{
             for (let k = -1; k < 2; k++){
                 const curr = board[coor.x+i] ? board[coor.x+i][coor.y+k] : null;
                 // If spot is out of bounds or
-                if(curr && curr.ship){
+                if(curr){
                     /* Checks if 
                         1.coordinates are not already occupied or diagonally adjacent to an occupied spot.
                         2.coordinates are adjacent to a different ship
                     */
-                    if(((i === 0 && k === 0) || (i !== 0 && k !== 0)) || curr.ship !== ship)
+                    if(((i === 0 && k === 0) || (i !== 0 && k !== 0)) || curr !== ship)
                         return false;
                 }
             }        
@@ -31,14 +31,14 @@ export const gameBoard = () =>{
     const removeShip = (coor) => {
         if(outOfBounds(coor))
             return false;
-        let res = board[coor.x][coor.y].ship;
-        board[coor.x][coor.y].ship = null;
-        return res;
+        let res = board[coor.x][coor.y];
+        board[coor.x][coor.y] = null;
+        return res; // Return the ship that was removed in order to put it back in the player's hands
     };
     
     const placeShip = (ship, coor) => {
         if(ship && checkValidity(ship, coor)){
-            board[coor.x][coor.y].ship = ship;
+            board[coor.x][coor.y] = ship;
             return true;
         }
         return false;
@@ -46,27 +46,25 @@ export const gameBoard = () =>{
     const isDestroyed = () => counter === 10;
     
     const receiveAttack = (coor) =>{
-        board[coor.x][coor.y].checked = true;
-        if(board[coor.x][coor.y].ship){
+        if(board[coor.x][coor.y]){
             let checks = []; // matrix of checked squares squares
-            board[coor.x][coor.y].ship.hit();
+            board[coor.x][coor.y].hit();
             // If hit, all diagonal adjacencies are also known to not be occupied
             for(let i = -1; i < 3; i += 2)
                 for(let k = -1; k < 3; k += 2)
-                    if(!outOfBounds({x: coor.x+i, y: coor.y+k})){
-                        board[coor.x+i][coor.y+k].checked = true;
+                    if(!outOfBounds({x: coor.x+i, y: coor.y+k}))
                         checks.push({x: coor.x+i, y: coor.y + k});
-                    }
-            if(board[coor.x][coor.y].ship.isSunk()){   
+                    
+            if(board[coor.x][coor.y].isSunk()){   
                 counter++;
                 // If ship is sunk, you can also be sure that all horizontal and vertical adjacent squares are also checked 
-                if(!outOfBounds({x: coor.x, y: coor.y + 1}) && !board[coor.x][coor.y+1].ship)
+                if(!outOfBounds({x: coor.x, y: coor.y + 1}) && !board[coor.x][coor.y+1])
                     checks.push({x: coor.x, y: coor.y + 1});
-                if(!outOfBounds({x: coor.x, y: coor.y - 1}) && !board[coor.x][coor.y-1].ship)
+                if(!outOfBounds({x: coor.x, y: coor.y - 1}) && !board[coor.x][coor.y-1])
                     checks.push({x: coor.x, y: coor.y - 1});
-                if(!outOfBounds({x: coor.x - 1, y: coor.y}) && !board[coor.x-1][coor.y].ship)
+                if(!outOfBounds({x: coor.x - 1, y: coor.y}) && !board[coor.x-1][coor.y])
                     checks.push({x: coor.x - 1, y: coor.y});
-                if(!outOfBounds({x: coor.x + 1, y: coor.y}) && !board[coor.x+1][coor.y].ship)
+                if(!outOfBounds({x: coor.x + 1, y: coor.y}) && !board[coor.x+1][coor.y])
                     checks.push({x: coor.x + 1, y: coor.y});
             }
             checks.unshift(coor);
@@ -78,10 +76,9 @@ export const gameBoard = () =>{
         let arr = [];
         for (let i = 0; i < 10; i++) {
             for (let k = 0; k < 10; k++) {
-                if(board[i][k].ship)
-                    arr.push(board[i][k].ship)
-                board[i][k].ship = null;
-                board[i][k].checked = null;
+                if(board[i][k])
+                    arr.push(board[i][k])
+                board[i][k] = null;
             }
         }
         counter = 0;
